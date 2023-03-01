255b15cf6d707b193875fb8b8097adcc006b7c4b
0027017473 <xiacakl@gmail.com>
2023-03-02 07:37:36 +0800

    fix:bug pre指针指向错误

--- linkedTables/linkTableMerge/linkTableMerge.java
+++ linkedTables/linkTableMerge/linkTableMerge.java
@@ -23,16 +23,16 @@ class linkTableMerge {
             
         while (cur1 != null && cur2 != null){ 
         if (cur1.val <= cur2.val ) {
-            pre.next = cur1.next;
+            pre.next = cur1;
             cur1 = cur1.next;
         } else { 
-            pre.next = cur2.next;
+            pre.next = cur2;
             cur2 = cur2.next;
         }  
         pre = pre.next;
         
     }
-     pre.next = cur1 != null ? cur1 : cur2;
+      pre.next = cur1 != null ? cur1 : cur2;
 
       return head;
 
